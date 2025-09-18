a92acbd0eb46dc963b586b14afd49e8e1679e63d
Hugoow0 <hugodev04@gmail.com>
2025-09-18 12:26:16 +0200

    link to mongoDB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 // Add CORS middleware
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -18,8 +21,7 @@ app.use((req, res, next) => {
     }
     next();
 });
-
-// Logs
+// Logs every request
 app.use((req, res, next) => {
     const t0 = Date.now();
     res.on("finish", () => {
@@ -35,6 +37,33 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("efrei-mycontacts").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.use((req, res) => {
     return res.status(404).json({
